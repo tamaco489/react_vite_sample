281fb799fa8f80e2d7b4bf8914c7cb59f4eda2d8
tamaco489 <tamaco489@gmail.com>
2025-02-08 14:22:26 +0900

    fix home page view

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { Account } from "@/features/account";
+
+/**
+ * path: /home
+ */
+export const HomePage = () => {
+  return (
+    <div className="mx-[10px]">
+      <Account />
+    </div>
+  );
+};
