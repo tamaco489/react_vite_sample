3467a1eede44dfdf9b2c114152e31289ac5b708c
tamaco489 <tamaco489@gmail.com>
2025-02-08 19:38:56 +0900

    fix base.mk

--- infra/make/base.mk
+++ infra/make/base.mk
@@ -1,17 +1,23 @@
-DEFAULT_ENV := dev
+ENV := dev
 
 ifeq ($(ENV),stg)
+	AWS_PROFILE := ""
 	AWS_ACCESS_KEY_ID := STG_ACCESS_KEY_ID
 	AWS_SECRET_ACCESS_KEY := STG_SECRET_ACCESS_KEY
 	AWS_REGION := ap-northeast-1
+	SOPS_KMS_ARN = "arn:aws:kms:ap-northeast-1:<account_id>:key/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 else ifeq ($(ENV),prd)
+	AWS_PROFILE := ""
 	AWS_ACCESS_KEY_ID := PRD_ACCESS_KEY_ID
 	AWS_SECRET_ACCESS_KEY := PRD_SECRET_ACCESS_KEY
 	AWS_REGION := ap-northeast-1
+	SOPS_KMS_ARN = "arn:aws:kms:ap-northeast-1:<account_id>:key/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 else
-	AWS_ACCESS_KEY_ID := $(shell echo $$AWS_ACCESS_KEY_ID)
-	AWS_SECRET_ACCESS_KEY := $(shell echo $$AWS_SECRET_ACCESS_KEY)
+	AWS_PROFILE := ""
+	AWS_ACCESS_KEY_ID := ""
+	AWS_SECRET_ACCESS_KEY := ""
 	AWS_REGION := ap-northeast-1
+	SOPS_KMS_ARN = "arn:aws:kms:ap-northeast-1:<account_id>:key/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 endif
 
 .PHONY: fmt init list show plan apply destroy
@@ -55,3 +61,14 @@ destroy:
 	export AWS_SECRET_ACCESS_KEY=$(AWS_SECRET_ACCESS_KEY) && \
 	export AWS_REGION=$(AWS_REGION) && \
 	terraform destroy
+
+
+.PHONY: encrypt-secret decrypt-secret
+
+# e.g. make encrypt-secret ENV=dev CREDENTIAL_FILE_NAME=web_front
+encrypt-secret:
+	@AWS_PROFILE=$(AWS_PROFILE) AWS_DEFAULT_REGION=$(AWS_REGION) sops --kms $(SOPS_KMS_ARN) tfvars/$(ENV)_$(CREDENTIAL_FILE_NAME).yaml
+
+# e.g. make decrypt-secret ENV=dev CREDENTIAL_FILE_NAME=web_front
+decrypt-secret:
+	@AWS_PROFILE=$(AWS_PROFILE) AWS_DEFAULT_REGION=$(AWS_REGION) sops --kms $(SOPS_KMS_ARN) -d tfvars/$(ENV)_$(CREDENTIAL_FILE_NAME).yaml
