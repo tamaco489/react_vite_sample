571d475c87d90a114b2d3bcf7305a3702de7d669
tamaco489 <tamaco489@gmail.com>
2025-02-09 14:45:47 +0900

    add description for oidc iam role

--- infra/credential/github/iam.tf
+++ infra/credential/github/iam.tf
@@ -1,5 +1,8 @@
+# Github Actions で AWS のdeployタスクを実行できるように、OIDC経由でロールを引き受けるための定義
+# CI/CD workflow で使用され、GitHub Actions からの OIDC 認証に基づいてアクセスを許可する。
 resource "aws_iam_role" "github_actions_oidc" {
   name               = "${local.fqn}-github-actions-oidc-role"
+  description        = "This IAM role allows GitHub Actions to assume the role via OIDC for performing deployment tasks on AWS, such as managing resources in the associated repository (repo: tamaco489/react_vite_sample). The role is used for CI/CD workflows and grants access based on OIDC authentication from GitHub Actions."
   assume_role_policy = data.aws_iam_policy_document.github_actions_oidc.json
 
   tags = {
